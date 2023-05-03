8bddfa5920413c7935b2aff8cc143f80496a9ebf
Atemyn <atemyn@mail.ru>
2023-05-03 18:58:21 +0700

    Реализован сеттер для MapView и метод initMap

--- app/src/main/java/com/example/mapofspotsdrawer/map/YandexMapManager.java
+++ app/src/main/java/com/example/mapofspotsdrawer/map/YandexMapManager.java
@@ -1,14 +1,22 @@
 package com.example.mapofspotsdrawer.map;
 
 import com.example.mapofspotsdrawer.model.Placemark;
+import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
 
 import java.util.List;
 
 public class YandexMapManager implements IMapManager{
     private static final YandexMapManager instance = new YandexMapManager();
+    private MapView mapView = null;
 
     private YandexMapManager() {
+    }
+
+    public void setMapView(MapView mapView) {
+        this.mapView = mapView;
     }
 
     public YandexMapManager getInstance() {
@@ -17,7 +25,11 @@ public class YandexMapManager implements IMapManager{
 
     @Override
     public void initMap(Point position, float zoom) {
-
+        mapView.getMap().move(
+                new CameraPosition(position, zoom, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 1),
+                null);
+        mapView.getMap().setRotateGesturesEnabled(true);
     }
 
     @Override
