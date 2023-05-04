6a856f6974576e99c5a0a181684b7ce6616a8f16
Atemyn <atemyn@mail.ru>
2023-05-04 17:22:50 +0700

    Создан фрагмент "Добавить спот"

---
+++ app/src/main/java/com/example/mapofspotsdrawer/ui/create_spot/CreateSpotFragment.java
@@ -0,0 +1,64 @@
+package com.example.mapofspotsdrawer.ui.create_spot;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mapofspotsdrawer.databinding.FragmentCreateSpotBinding;
+import com.example.mapofspotsdrawer.map.YandexMapManager;
+import com.example.mapofspotsdrawer.ui.favorite.FavoriteViewModel;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+
+public class CreateSpotFragment extends Fragment {
+    private FragmentCreateSpotBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        MapKitFactory.initialize(this.requireContext());
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CreateSpotViewModel createSpotViewModel =
+                new ViewModelProvider(this).get(CreateSpotViewModel.class);
+
+        binding = FragmentCreateSpotBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        YandexMapManager.getInstance().setMapView(binding.mapview);
+        return root;
+    }
+
+    @Override
+    public void onStop() {
+        binding.mapview.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        binding.mapview.onStart();
+        YandexMapManager mapManager = YandexMapManager.getInstance();
+        // TODO Переделать, чтобы состояние сохранялось при повороте экрана.
+        mapManager.moveMapTo(new Point(55.751574, 37.573856), 5.0f);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/com/example/mapofspotsdrawer/ui/create_spot/CreateSpotViewModel.java
@@ -0,0 +1,7 @@
+package com.example.mapofspotsdrawer.ui.create_spot;
+
+import androidx.lifecycle.ViewModel;
+
+public class CreateSpotViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
