d41d5b86854ae1eb25df10eda0db268bf0889185
Atemyn <atemyn@mail.ru>
2023-05-04 16:49:45 +0700

    Фрагмент "О приложении" добавлен в боковое меню

--- app/src/main/java/com/example/mapofspotsdrawer/ui/about/AboutFragment.java
+++ app/src/main/java/com/example/mapofspotsdrawer/ui/about/AboutFragment.java
@@ -22,14 +22,14 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ProfileViewModel profileViewModel =
-                new ViewModelProvider(this).get(ProfileViewModel.class);
+        AboutViewModel aboutViewModel =
+                new ViewModelProvider(this).get(AboutViewModel.class);
 
         binding = FragmentAboutBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textAbout;
-        profileViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        aboutViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
