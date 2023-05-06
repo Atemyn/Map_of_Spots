66603fd99736eb69b2020767944a31cf472ea8bf
Atemyn <atemyn@mail.ru>
2023-05-06 17:05:04 +0700

    Удален ненужный импорт

--- app/src/main/java/com/example/mapofspotsdrawer/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/mapofspotsdrawer/ui/profile/ProfileFragment.java
@@ -11,7 +11,6 @@ import androidx.fragment.app.Fragment;
 import com.example.mapofspotsdrawer.R;
 import com.example.mapofspotsdrawer.databinding.FragmentProfileBinding;
 import com.example.mapofspotsdrawer.ui.auth.AuthFragment;
-import com.example.mapofspotsdrawer.ui.auth.login.LoginFragment;
 
 public class ProfileFragment extends Fragment {
 
