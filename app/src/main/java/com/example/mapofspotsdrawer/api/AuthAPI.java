491452bfb9b86b12e5bfe4773e3225d05ed5894d
Atemyn <atemyn@mail.ru>
2023-05-06 21:35:22 +0700

    Добавлено API для обращения к серверу

---
+++ app/src/main/java/com/example/mapofspotsdrawer/api/AuthAPI.java
@@ -0,0 +1,12 @@
+package com.example.mapofspotsdrawer.api;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface AuthAPI {
+    @POST("/api/auth/register")
+    Call<ResponseBody> registerUser(@Body RequestBody requestBody);
+}
