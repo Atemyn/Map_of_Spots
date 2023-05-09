61338110934aad88e5997e81a2824713abe63466
Atemyn <atemyn@mail.ru>
2023-05-09 20:04:40 +0700

    Написан класс ViewModel для ProfileDataFragment

--- app/src/main/java/com/example/mapofspotsdrawer/ui/profile/ProfileDataViewModel.java
+++ app/src/main/java/com/example/mapofspotsdrawer/ui/profile/ProfileDataViewModel.java
@@ -2,6 +2,52 @@ package com.example.mapofspotsdrawer.ui.profile;
 
 import androidx.lifecycle.ViewModel;
 
-public class ProfileDataViewModel extends ViewModel {
+import java.time.LocalDate;
 
+public class ProfileDataViewModel extends ViewModel {
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthday;
+    private LocalDate registrationDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
 }
