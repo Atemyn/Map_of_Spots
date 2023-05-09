3c0138c9c909be0e21c23296f916d3e71b1d05d9
Atemyn <atemyn@mail.ru>
2023-05-09 16:38:45 +0700

    Написан класс интерфейса UserAPI

--- app/src/main/java/com/example/mapofspotsdrawer/api/UserAPI.java
+++ app/src/main/java/com/example/mapofspotsdrawer/api/UserAPI.java
@@ -1,4 +1,12 @@
 package com.example.mapofspotsdrawer.api;
 
+import com.example.mapofspotsdrawer.model.User;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
 public interface UserAPI {
+    @GET("api/get-user-info")
+    Call<User> getData(@Header("Authorization") String jwtToken);
 }
