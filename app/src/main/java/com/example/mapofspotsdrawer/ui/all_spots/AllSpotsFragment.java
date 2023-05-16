6c03ef1ea93bd6f085ea216980756897c2022496
Atemyn <atemyn@mail.ru>
2023-05-16 18:55:25 +0700

    Написан метод для получения всех спотов с сервера

--- app/src/main/java/com/example/mapofspotsdrawer/ui/all_spots/AllSpotsFragment.java
+++ app/src/main/java/com/example/mapofspotsdrawer/ui/all_spots/AllSpotsFragment.java
@@ -4,19 +4,34 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.mapofspotsdrawer.R;
+import com.example.mapofspotsdrawer.api.SpotAPI;
+import com.example.mapofspotsdrawer.api.SpotTypeAPI;
 import com.example.mapofspotsdrawer.databinding.FragmentAllSpotsBinding;
 import com.example.mapofspotsdrawer.map.YandexMapManager;
+import com.example.mapofspotsdrawer.model.Spot;
+import com.example.mapofspotsdrawer.model.SpotType;
+import com.example.mapofspotsdrawer.retrofit.RetrofitService;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AllSpotsFragment extends Fragment {
 
     private FragmentAllSpotsBinding binding;
+
+    private AllSpotsViewModel allSpotsViewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,14 +41,77 @@ public class AllSpotsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        AllSpotsViewModel allSpotsViewModel =
-                new ViewModelProvider(this).get(AllSpotsViewModel.class);
+        allSpotsViewModel = new ViewModelProvider(this).get(AllSpotsViewModel.class);
 
         binding = FragmentAllSpotsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         YandexMapManager.getInstance().setMapView(binding.mapview);
+
+        List<Spot> spots = allSpotsViewModel.getSpots();
+        if (spots != null && spots.size() != 0) {
+            showSpotsOnMap(spots);
+        }
+        else {
+            getSpotTypes();
+        }
+
         return root;
+    }
+
+    private void showSpotsOnMap(List<Spot> spots) {
+
+    }
+
+    private void getSpotTypes() {
+        RetrofitService retrofitService = new RetrofitService(getString(R.string.server_url));
+
+        binding.progressBar.setVisibility(View.VISIBLE);
+
+        // Создание API для совершения запроса к серверу.
+        SpotAPI spotAPI = retrofitService.getRetrofit().create(SpotAPI.class);
+
+        spotAPI.getAllSpots()
+                .enqueue(new Callback<List<Spot>>() {
+                    @Override
+                    public void onResponse(@NonNull Call<List<Spot>> call,
+                                           @NonNull Response<List<Spot>> response) {
+                        if (response.isSuccessful()) {
+                            if (processSpotsResponseBody(response.body())) {
+                                requireActivity().runOnUiThread(() ->
+                                        binding.progressBar.setVisibility(View.GONE));
+                                showSpotsOnMap(response.body());
+                            }
+                            else {
+                                disableProgressBarAndShowNotification("Ошибка получения тела ответа");
+                            }
+                        }
+                        else {
+                            disableProgressBarAndShowNotification("Ошибка обработки запроса на сервере");
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<List<Spot>> call,
+                                          @NonNull Throwable t) {
+                        disableProgressBarAndShowNotification("Ошибка отправки запроса на сервер");
+                    }
+                });
+    }
+
+    private boolean processSpotsResponseBody(List<Spot> spots) {
+        if (spots != null && spots.size() != 0) {
+            allSpotsViewModel.setSpots(spots);
+            return true;
+        }
+        return false;
+    }
+
+    private void disableProgressBarAndShowNotification(String message) {
+        requireActivity().runOnUiThread(() ->
+                binding.progressBar.setVisibility(View.GONE));
+        Toast.makeText(getActivity(),
+                message, Toast.LENGTH_LONG).show();
     }
 
     @Override
