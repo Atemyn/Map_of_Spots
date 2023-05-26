5efe540ac5df4f414b064a380faf6a040f5dcbc2
Atemyn <atemyn@mail.ru>
2023-05-27 01:16:39 +0700

    Добавлено 2 метода для изменения состояния лайка и избранного для спота в LikesFavoritesAPI

--- app/src/main/java/com/example/mapofspotsdrawer/api/LikesFavoritesAPI.java
+++ app/src/main/java/com/example/mapofspotsdrawer/api/LikesFavoritesAPI.java
@@ -2,13 +2,24 @@ package com.example.mapofspotsdrawer.api;
 
 import com.example.mapofspotsdrawer.model.SpotUserDto;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.PATCH;
 import retrofit2.http.Path;
 
 public interface LikesFavoritesAPI {
     @GET("/api/spots-users/get-info/{spotId}")
     Call<SpotUserDto> getLikesAndFavoritesForSpot(@Path("spotId") Long spotId,
+                                                  @Header("Authorization") String jwtToken);
+
+    @PATCH("/api/spots-users/change-like-state/{spotId}")
+    Call<ResponseBody> changeLikeStateForSpot(@Path("spotId") Long spotId,
+                                              @Header("Authorization") String jwtToken);
+
+    @PATCH("/api/spots-users/change-favorite-state/{spotId}")
+    Call<ResponseBody> changeFavoriteStateForSpot(@Path("spotId") Long spotId,
                                                   @Header("Authorization") String jwtToken);
 }
